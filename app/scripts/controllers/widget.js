98fd554453251e1279284b8fbea995ccc0d8685b
Moka- <ebmoha@gmail.com>
2016-04-13 17:23:18 +0300

    mid day checkin

--- app/scripts/controllers/widget.js
+++ app/scripts/controllers/widget.js
@@ -3,13 +3,35 @@
 angular.module('widget')
     .controller('WidgetController', function($scope, $wix, feedbacksDb) {
 
-        $scope.newComment = {};
-        $scope.feedbacks = feedbacksDb.feedbackss;
+        $scope.newComment = {
+            content: ""
+        };
 
-        /*if ($wix.Utils.getViewMode() !== 'standalone') {
-            $scope.instanceId = $wix.Utils.getInstanceId();
-            $scope.instance = $wix.Utils.getInstance();
-        }*/
+        $scope.widgetInstance;
+
+        $scope.$watch(
+            "widgetInstance",
+            function change(newValue, oldValue) {
+              if (Number.isInteger(newValue)) {
+                feedbacksDb.getFeedbacks($scope.widgetInstance).then(function(d) {
+                    $scope.data = d;
+                });
+              }
+            }
+        );
+
+        $scope.submit = function() {
+            if ($scope.newComment) {
+                $scope.data.push({
+                    "widget": 10,
+                    "name": "Judith Wallace",
+                    "content": "Morbi a ipsum. Integer a nibh. In quis justo.",
+                    "publish_date": "19.10.2015",
+                    "publish_time": "15:43",
+                    "image": "http://dummyimage.com/231x134.gif/5fa2dd/ffffff"
+                });
+            }
+        };
     })
     .directive('feedback', function($interpolate) {
         return {
@@ -20,14 +42,13 @@ angular.module('widget')
             templateUrl: 'views/templates/feedback.html '
         };
     })
-    .factory('feedbacksDb', ['$http', function($http) {
-
-        var obj = {
-            feedbackss: null
-        };
-
-        $http.get('/dummy_data/feedbacks.json').success(function(response) {
-            obj.feedbackss = response;
-        });
-        return obj;
+    .service('feedbacksDb', ['$http', '$filter', function($http, $filter) {
+        this.getFeedbacks = function getFeedbacks(widgetInstance) {
+            var promise = $http.get('/dummy_data/feedbacks.json').then(function(response) {
+                return $filter('filter')(response.data, {
+                    widget: widgetInstance
+                });
+            });
+            return promise;
+        }
     }]);
