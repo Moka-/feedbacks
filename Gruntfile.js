9b1ed3d102e27d2d8f2abe789d4959573b5b9fad
SyDroX <syodruk.kostya@gmail.com>
2016-04-19 19:09:58 +0300

    uploaded my version of these shinanigans

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    //grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     //grunt.registerTask('test', ['jshint', 'qunit']);
