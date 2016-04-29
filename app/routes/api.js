3661a4a949efef1e9c8e9859070f13ff829eb7a3
SyDroX <sydoruk.kostya@gmail.com>
2016-04-29 19:52:58 +0300

    Merge branches 'Kostya' and 'master' of https://github.com/Moka-/feedbacks into Kostya
    # Conflicts:
    #	app/routes/api.js
    
    Added feedbacks.add, hopefully it works.

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,138 +1,149 @@
 'use strict';
 
-var fs = require('fs');
-var dummy_visitors = JSON.parse(fs.readFileSync('./app/dummy_data/visitors.json', 'utf8'));
-var dummy_feedbacks = JSON.parse(fs.readFileSync('./app/dummy_data/feedbacks.json', 'utf8'));
+var dal = require('../dal/dal');
+var uuid = require('node-uuid');
+const https = require('https');
 
-var mysql = require('mysql');
-var pool = mysql.createPool({
-    connectionLimit: 100, //important
-    host: 'wix-feedbacks-test.cohruqtd5dnp.us-west-2.rds.amazonaws.com',
-    port: '1337',
-    user: 'feedback',
-    password: 'Aa123456',
-    database: 'wix_feedbacks_test',
-    debug: false
-});
-
-function handle_database(req, res) {
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            connection.release();
-            console.log(err);
-            res.json(err);
-            return;
+module.exports = {
+    visitors: {
+        list: function (req, res) {
+            dal.visitors.list(req.params, function (err, result) {
+                res.json(result);
+            });
+        },
+        view: function (req, res) {
+            dal.visitors.view(req.params, function (err, result) {
+                res.json(result);
+            });
+        },
+        add: function (req, res) {
+            req.query = 'INSERT INTO visitors SET';
+            handle_database(req, res);
+        },
+        update: function (req, res) {
+            handle_database(req, res);
+        },
+        delete: function (req, res) {
+            req.query = 'DELETE visitors WHERE id ='
+            handle_database(req, res);
         }
+    },
+    feedbacks: {
+        list: function (req, res) {
+            var params = {widget_id: req.params.widget_id};
+            dal.feedbacks.list(params, function (err, results) {
+                res.json(results);
+            });
+        },
+        view: function (req, res) {
 
-        console.log('connected as id ' + connection.threadId);
+        },
+        add: function (req, res) {
+            /*
+             widget_id: $scope.$parent.widget_id,
+             comment: $scope.new_feedback.comment,
+             rating: $scope.new_feedback.rating,
+             id_token: $scope.logged_user.id_token,
+             full_name: $scope.logged_user.full_name,
+             avatar_url: $scope.logged_user.image_url
+             */
 
-        /*connection.config.queryFormat = function (query, values) {
-            if (!values) return query;
-            return query.replace(/:(\w+)/g, function (txt, key) {
-                if (values.hasOwnProperty(key)) {
-                    return this.escape(values[key]);
-                }
-                return txt;
-            }.bind(this));
-        };*/
-        connection.query(req.sql, req.params, function (err, rows) {
-            connection.release();
-            if (!err) {
-                res.json(rows);
-            }
-        });
+            var feedback = req.body;
 
-        connection.on('error', function (err) {
-            console.log(err);
-            res.json(err);
-            return;
-        });
-    });
-}
+            feedback.id = uuid.v4();
+            feedback.created_on = new Date().toISOString();
+            var tokenInfoUrl = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + params.id_token;
 
-var widgetAvgQuery = 'SELECT widget_id,ROUND(AVG(t.rating),1) FROM feedbacks t GROUP BY widget_id';
-var insertFeedback = 'INSERT INTO `feedbacks` SET ?';
-var getWidgetData = 'SELECT f.*, v.display_name, v.avatar_url ' +
-                    'FROM `feedbacks` f,`visitors` v ' +
-                    'WHERE  f.visitor_id = v.id ' +
-                    'AND ?';
-var dummy_visitors = JSON.parse(fs.readFileSync('./app/dummy_data/visitors.json', 'utf8'));
-var dummy_feedbacks = JSON.parse(fs.readFileSync('./app/dummy_data/feedbacks.json', 'utf8'));
+            https.get(tokenInfoUrl, function (googleRes) {
+                console.log('statusCode: ', googleRes.statusCode);
+                console.log('headers: ', googleRes.headers);
+                console.log('email: ', googleRes.body.email);
+                feedback.visitor_id = res.body.email;
 
-exports.visitors = {
-    list: function (req, res) {
-        req.sql = 'SELECT * FROM `visitors`';
-        handle_database(req, res);
+                dal.visitors.view(feedback.visitor_id, function (err, user) {
+                    if (user.length == 0) {
+                        var visitor = {
+                            google_id_token: req.body.id_token,
+                            display_name: req.body.full_name,
+                            avatar_url: googleRes.body.picture,
+                            id: res.body.email
+                        };
+                        dal.visitors.add(visitor, function (err, result) {
+                            dal.feedbacks.add(params, function (err, results) {
+                                res.json(results);
+                            });
+                        });
+                    } else {
+                        dal.feedbacks.add(params, function (err, results) {
+                            res.json(results);
+                        });
+                    }
+                });
+            });
+            // var params = {
+            //     id:"fdsfdsfssd",
+            // comment
+            //     :
+            //     "this is a test",
+            // comment_title
+            //     :
+            //     "Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.",
+            // created_on
+            //     :
+            //     "2015-07-28T21:00:00.000Z",
+            // edited_on
+            //     :
+            //     "2016-04-25T21:00:00.000Z",
+            // rating
+            //     :
+            //     10,
+            // visitor_id
+            //     :
+            //     "8884d4e3-2fda-4dbe-a0b8-605fccaa3b0c",
+            // widget_id
+            //     :
+            //     "bcac1c8a-3b11-4374-aff7-e865a14c2681comp-imxne0xw"
+            // }
+        },
+        update: function (req, res) {
+            res.render('widget');
+        },
+        delete: function (req, res) {
+            res.render('widget');
+        }
     },
-    view: function (req, res) {
-        req.sql = 'SELECT * FROM `visitors` WHERE ?';
-        handle_database(req, res);
+    widgets: {
+        list: function (req, res) {
+            res.json(dummy_visitors);
+        },
+        view: function (req, res) {
+            res.render('widget');
+        },
+        add: function (req, res) {
+            res.render('widget');
+        },
+        update: function (req, res) {
+            res.render('widget');
+        },
+        delete: function (req, res) {
+            res.render('widget');
+        }
     },
-    add: function (req, res) {
-        req.sql = 'INSERT INTO `visitors` SET';
-        handle_database(req, res);
-    },
-    update: function (req, res) {
-        handle_database(req, res);
-    },
-    delete: function (req, res) {
-        req.sql = 'DELETE `visitors` WHERE `id` =';
-        handle_database(req, res);
-    }
-};
-
-exports.feedbacks = {
-    list: function (req, res) {
-        req.sql = getWidgetData;
-        handle_database(req, res);
-    },
-    view: function (req, res) {
-        res.render('widget');
-    },
-    add: function (req, res) {
-        req.sql = insertFeedback;
-        handle_database(req, res);
-    },
-    update: function (req, res) {
-        res.render('widget');
-    },
-    delete: function (req, res) {
-        res.render('widget');
-    }
-};
-
-exports.widgets = {
-    list: function (req, res) {
-        res.json(dummy_visitors);
-    },
-    view: function (req, res) {
-        res.render('widget');
-    },
-    add: function (req, res) {
-        res.render('widget');
-    },
-    update: function (req, res) {
-        res.render('widget');
-    },
-    delete: function (req, res) {
-        res.render('widget');
-    }
-};
-
-exports.sites = {
-    list: function (req, res) {
-        res.json(dummy_visitors);
-    },
-    view: function (req, res) {
-        res.render('widget');
-    },
-    add: function (req, res) {
-        res.render('widget');
-    },
-    update: function (req, res) {
-        res.render('widget');
-    },
-    delete: function (req, res) {
-        res.render('widget');
+    sites: {
+        list: function (req, res) {
+            res.json(dummy_visitors);
+        },
+        view: function (req, res) {
+            res.render('widget');
+        },
+        add: function (req, res) {
+            res.render('widget');
+        },
+        update: function (req, res) {
+            res.render('widget');
+        },
+        delete: function (req, res) {
+            res.render('widget');
+        }
     }
 };
